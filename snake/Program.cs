a182742291e5db1d113a7a5c32684b924e3d38b4
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2022-07-23 17:55:37 +0300

    priject file saved

--- snake/Program.cs
+++ snake/Program.cs
@@ -14,11 +14,5 @@
 
             Console.ReadLine();
         }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
     }
 }
