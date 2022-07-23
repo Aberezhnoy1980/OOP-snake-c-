9c55405bc30a850b5b538292e8caffb167d9313c
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2022-07-23 20:05:19 +0300

    added class HorLine

---
+++ snake/HorizontalLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace snake
+{
+    public class HorizontalLine
+    {
+        List<Point> plist;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            plist = new List<Point>();
+            for(int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                plist.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach(Point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
+
